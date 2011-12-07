ae3f4a85c806393d29a4301c5461c1b90c312f13
kishore k <kishore.k@directi.com>
2011-12-07 10:45:03 +0530

    Added script to generate thumbnails

---
+++ gen_thumbs.sh
@@ -0,0 +1,21 @@
+# To create thumbnails out of all the jpg s in the directory 
+# 
+# 
+# set -v -x 
+
+THUMBS_DIR="thumbs_${RAND}"
+
+
+echo "Creating the ${THUMBS_DIR}..." 
+echo "Starting conversion " 
+
+for fil in `ls *.jpg`  ; 
+do 
+name=${fil%.*}; 
+convert -thumbnail 200 $fil ${THUMBS_DIR}/${name}_thumb.jpg; 
+done 
+echo "Done converting" 
+echo "Thumbs in the dir ${THUMBS_DIR}" 
+
+
+
